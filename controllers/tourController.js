5ab1427199cee0e49b3960f86dca923e176e00c9
Okwudili David <michealdavid910@gmail.com>
2019-11-08 00:23:07 +0100

    update: added new features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,29 +8,56 @@ exports.getAllTours = async (req, res) => {
         // BUILDING THE QUERY (filtering)
         const queryObj = { ...req.query }; // 
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(el => delete queryObj[el]); 
-
+        excludedFields.forEach(el => delete queryObj[el]);
+        debugger;
         // ADVANCED FILTERING
         let queryStr = JSON.stringify(queryObj);
-        //build the queryStr and replace to put the '$' before them.
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        queryStr = JSON.parse(queryStr); 
-
-        // use without calling "await, so it can return a queryable object"
-        const query = Tour.find(queryStr);
-
-        // SORTING Feature
-
-
-        // const query = Tour.find()
-        //     .where('duration').equals(5)
-        //     .where('difficulty').equals('easy');
-
+        //build the queryStr and replace to put the '$' before them. 
         // const tours = await Tour.find({ difficulty: 'easy', duration: { $gte: 5 } });
         // (greater than or equal => 'gte')
         // (greater than => 'gt')
         // (less than or equal => 'lte' )
         // (less than => 'lt')
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        queryStr = JSON.parse(queryStr);
+
+        // use without calling "await, so it can return a queryable object"
+        let query = Tour.find(queryStr);
+
+        // SORTING Feature
+        if (req.query.sort) {
+            // .sort('price ratingsAverage')
+            const sortBy = req.query.sort.split(',').join(' ');
+            query.sort(sortBy);
+        }
+        else {
+            // if a sort value wasnt passed, ll be sorted with a default
+            // will start from the latest createdAt
+            query.sort('-createdAt');
+        }
+
+        // FIELDS Feature
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query.select(fields);
+        }
+        else {
+            // remove the returning '__v' property
+            query.select('-__v');
+        }
+
+        // PAGINATION
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if (req.query.page) {
+            // throw an error when the selected page isnt available
+            const totalDoc = await Tour.countDocuments();
+            if (skip >= totalDoc) throw new Error('This page does not exist...');
+        }
 
         // execute query
         const tours = await query;
