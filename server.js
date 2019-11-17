f7e9d7eaee7f57753be5c5b695c3ab55dce8c836
Okwudili David <michealdavid910@gmail.com>
2019-11-17 02:10:45 +0100

    testing CI

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-mongoose.connect(process.env.DB_LOCAL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-}).then(() => {
-    // console.log(con);
-    console.log('Database connected...');
-}).catch(err => { console.log(err); });
+// before pushing to git, comment out this code below....
+// mongoose.connect(process.env.DB_LOCAL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     // console.log(con);
+//     console.log('Database connected...');
+// }).catch(err => { console.log(err); });
 
 // console.log(app.get('env'));
 // console.log(process.env);
