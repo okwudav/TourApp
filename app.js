ae416f3b18a8e675343e1579aa84e3e5fe79ef92
Okwudili David <michealdavid910@gmail.com>
2019-11-27 01:14:26 +0100

    added: code improvement

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRoute = require('./routes/tourRoute');
 const userRoute = require('./routes/userRoute');
 
@@ -21,30 +23,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRoute);
 app.use('/api/v1/users', userRoute);
 
-// MIDDLEWARE to check routes that wr not executes above b4 reaching here
+// MIDDLEWARE to check ALL(*) routes that wr not executes above b4 reaching here
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'failed',
-    //     message: `Can't find ${req.originalUrl} on this server!`
-    // });
-    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-    err.statusCode = 404;
-    err.status = 'failed';
-
-    next(err);
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 // ERROR MIDDLEWAEW 
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
-
-    next();
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
